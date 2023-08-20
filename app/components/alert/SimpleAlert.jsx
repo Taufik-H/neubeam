0d5dbafb33d5ef8506dd04d3a27b2b7274bb9980
Taufik-H <taufikrpl1@gmail.com>
2023-08-20 13:16:34 +0700

    feat:simple alert component

---
+++ app/components/alert/SimpleAlert.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CodeBlock from "@/components/highlighter/CodeBlock";
+import UiSimpleAlert from "@/ui/alert/UiSimpleAlert";
+import ReactDOMServer from "react-dom/server";
+import beautify from "html";
+const SimpleAlert = () => {
+  const rawCode = ReactDOMServer.renderToString(
+    <UiSimpleAlert title="My Title" />
+  );
+  const myCodeString = beautify.prettyPrint(rawCode, { indent_size: 2 });
+  return (
+    <div>
+      <CodeBlock code={myCodeString} language="html" />
+    </div>
+  );
+};
+
+export default SimpleAlert;
