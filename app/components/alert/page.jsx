46852dd7f34965cacb4a34d211d0045a3a728070
Taufik-H <taufikrpl1@gmail.com>
2023-08-20 13:16:22 +0700

    rename

---
+++ app/components/alert/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import SimpleAlert from "./SimpleAlert";
+const page = () => {
+  return (
+    <div>
+      <SimpleAlert />
+    </div>
+  );
+};
+
+export default page;
