888d988b32beed63f992ed4470a1445dc60cd1aa
Taufik-H <taufikrpl1@gmail.com>
2023-08-28 13:03:17 +0700

    add:nunito

--- app/components/alert/page.jsx
+++ app/components/alert/page.jsx
@@ -1,9 +1,22 @@
 "use client";
 import React from "react";
 import SimpleAlert from "./SimpleAlert";
+import { Nunito } from "next/font/google";
+import Navbar from "@/components/Navbar";
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  weights: [400, 700],
+});
 const page = () => {
   return (
-    <div>
+    <div className={nunito.className}>
+      <div className="w-full border-b border-slate-200">
+        <Navbar />
+      </div>
+      <p className="font-bold text-slate-500 my-10 mx-5">
+        Home Component <span className="text-slate-900">Alert</span>
+      </p>
       <SimpleAlert />
     </div>
   );
