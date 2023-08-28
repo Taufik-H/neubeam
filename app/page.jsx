a397151875e8358ea1b56473e5e8404f4fa1086d
Taufik-H <taufikrpl1@gmail.com>
2023-08-28 13:02:56 +0700

    feat:componentDAta

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,15 @@ import HeroSection from "@/components/sections/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
+  const componentsData = [
+    { label: "Alert", count: 4 },
+    { label: "Buttons", count: 4 },
+    { label: "Cards", count: 7 },
+    { label: "Badges", count: 3 },
+    { label: "Footers", count: 6 },
+    { label: "Forms", count: 8 },
+    { label: "Breadcrumbs", count: 2 },
+  ];
   return (
     <main className="w-full">
       <div className="absolute h-screen w-screen -z-10 overflow-hidden">
@@ -27,26 +36,20 @@ export default function Home() {
 
       <section className="my-10 px-5 ">
         <div className="flex flex-wrap gap-3 justify-center">
-          {[
-            "Buttons",
-            "Cards",
-            "Badges",
-            "Footers",
-            "Forms",
-            "Breadcrumbs",
-          ].map((label) => (
-            <div
+          {componentsData.map(({ label, count }) => (
+            <a
+              href={`/components/${label.toLocaleLowerCase()}`}
               key={label}
-              className=" bg-slate-500 w-full  md:w-32 lg:w-72 relative h-32 rounded-lg  "
+              className="bg-slate-500 w-full md:w-32 lg:w-72 relative h-32 rounded-lg"
             >
-              <div className="absolute text-slate-900 font-thin  justify-start gap-3  hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-start bg-slate-50 border  rounded-lg border-slate-500">
+              <div className="absolute text-slate-900 font-thin justify-start gap-3 hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-start bg-slate-50 border rounded-lg border-slate-500">
                 <div className="w-10 h-10 bg-slate-400 rounded-lg"></div>
                 <div className="">
                   <p className="font-bold">{label}</p>
-                  <p className="text-xs text-blue-500">4 components</p>
+                  <p className="text-xs text-blue-500">{count} components</p>
                 </div>
               </div>
-            </div>
+            </a>
           ))}
         </div>
       </section>
