4171d371d2d6abbd4ebf4d7cd53f67f593ffca21
Taufik-H <taufikrpl1@gmail.com>
2023-08-28 13:03:32 +0700

    remove:title

--- app/components/alert/SimpleAlert.jsx
+++ app/components/alert/SimpleAlert.jsx
@@ -4,9 +4,7 @@ import UiSimpleAlert from "@/ui/alert/UiSimpleAlert";
 import ReactDOMServer from "react-dom/server";
 import beautify from "html";
 const SimpleAlert = () => {
-  const rawCode = ReactDOMServer.renderToString(
-    <UiSimpleAlert title="My Title" />
-  );
+  const rawCode = ReactDOMServer.renderToString(<UiSimpleAlert />);
   const myCodeString = beautify.prettyPrint(rawCode, { indent_size: 2 });
   return (
     <div>
