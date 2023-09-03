47f48a0486ed3efe6b47610cbbdd1422397c096e
Taufik-H <taufikrpl1@gmail.com>
2023-09-03 12:58:41 +0700

    responsive preview simple button position

--- ui/button/UiSimpleButton.jsx
+++ ui/button/UiSimpleButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function UiSimpleButton() {
   return (
-    <div className="flex gap-3 h-screen justify-center items-center w-full">
+    <div className="flex gap-3 h-screen justify-center items-center md:flex-row flex-col w-full">
       <div className="w-36">
         <div className=" bg-slate-800 w-full relative h-8   ">
           <p className="absolute  justify-center gap-3  text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
