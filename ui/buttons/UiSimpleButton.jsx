fcf032a6ff2997ba71e48d93fb6c2e29ad835c4d
Taufik-H <taufikrpl1@gmail.com>
2023-09-26 13:53:56 +0700

    edit:p to button

--- ui/buttons/UiSimpleButton.jsx
+++ ui/buttons/UiSimpleButton.jsx
@@ -5,32 +5,32 @@ function UiSimpleButton() {
     <div className="flex gap-3 h-screen justify-center items-center md:flex-row flex-col w-full">
       <div className="w-36">
         <div className=" bg-slate-800 w-full relative h-8   ">
-          <p className="absolute  justify-center gap-3  text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
+          <button className="absolute  justify-center gap-3  text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
             Button
-          </p>
+          </button>
         </div>
       </div>
       <div className="w-36">
         <div className=" bg-slate-800 w-full relative h-8 rounded-md  ">
-          <p className="absolute  justify-center gap-3 rounded-md text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
+          <button className="absolute  justify-center gap-3 rounded-md text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
             Button rounded-md
-          </p>
+          </button>
         </div>
       </div>
 
       <div className="w-36">
         <div className=" bg-slate-800 w-full relative h-8 rounded-lg  ">
-          <p className="absolute  justify-center gap-3 rounded-lg text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
+          <button className="absolute  justify-center gap-3 rounded-lg text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
             Button rounded-lg
-          </p>
+          </button>
         </div>
       </div>
 
       <div className="w-36">
         <div className=" bg-slate-800 w-full relative h-8 rounded-full  ">
-          <p className="absolute  justify-center gap-3 rounded-full text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
+          <button className="absolute  justify-center gap-3 rounded-full text-xs hover:-top-0 transition-all duration-200 hover:bg-slate-100 lg:cursor-pointer hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-50 border w-53  border-slate-800">
             Button rounded-full
-          </p>
+          </button>
         </div>
       </div>
     </div>
