ce5990e982da7cd527a460a64bea15c4737e1ff1
Taufik-H <taufikrpl1@gmail.com>
2023-10-14 22:56:54 +0700

    add: more component data

--- constant/componentList.js
+++ constant/componentList.js
@@ -1,15 +1,31 @@
 // componentsList.js
+import AlertWithAction from "@/ui/alert/AlertWithAction";
 import UiSimpleAlert from "@/ui/alert/UiSimpleAlert";
+import SimpleBadge from "@/ui/badges/SimpleBadge";
 import UiSimpleButton from "@/ui/buttons/UiSimpleButton";
 import UiSimpleCard from "@/ui/cards/UiSimpleCard";
+import SimpleFooter from "@/ui/footers/SimpleFooter";
+import SimpleInput from "@/ui/inputs/SimpleInput";
+import SimpleNavbar from "@/ui/navbars/SimpleNavbar";
+import SimplePagination from "@/ui/pagination/SimplePagination";
 
 export const alerts = [
   {
     component: <UiSimpleAlert />,
     name: "Simple Alert",
   },
+  {
+    component: <AlertWithAction />,
+    name: "Alert with action",
+  },
 ];
 
+export const badges = [
+  {
+    component: <SimpleBadge />,
+    name: "Simple Badge",
+  },
+];
 export const buttons = [
   {
     component: <UiSimpleButton />,
@@ -20,5 +36,33 @@ export const cards = [
   {
     component: <UiSimpleCard />,
     name: "Simple Card",
+  },
+];
+
+export const footers = [
+  {
+    component: <SimpleFooter />,
+    name: "Simple Footer",
+  },
+];
+
+export const inputs = [
+  {
+    component: <SimpleInput />,
+    name: "Simple Input",
+  },
+];
+
+export const navbar = [
+  {
+    component: <SimpleNavbar />,
+    name: "Simple Navbar",
+  },
+];
+
+export const paginations = [
+  {
+    component: <SimplePagination />,
+    name: "Simple Pagination",
   },
 ];
