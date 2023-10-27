ee1d9d998b319d4a7fde3a47c37d587ca9067b07
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-10-27 18:03:29 +0700

    add component simple input

--- ui/inputs/SimpleInput.jsx
+++ ui/inputs/SimpleInput.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const SimpleInput = () => {
-  return <div>SimpleInput</div>;
+  return (
+    <div className="p-4">
+      <label for="text" className="text-slate-900">
+        Email
+      </label>
+      <input
+        type="text"
+        placeholder="Text"
+        className="border-2 outline-slate-900 active:outline-slate-900 border-slate-200 w-full p-2"
+      />
+    </div>
+  );
 };
 
 export default SimpleInput;
