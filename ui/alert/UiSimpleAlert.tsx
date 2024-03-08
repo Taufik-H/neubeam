50fb9a8b60f172b9fe88f614cb1b9189c91759b3
Taufik-H <taufikrpl1@gmail.com>
2024-03-08 21:29:47 +0700

    Add UiSimpleAlert component

---
+++ ui/alert/UiSimpleAlert.tsx
@@ -0,0 +1,43 @@
+const UiSimpleAlert = () => {
+  return (
+    <div className="flex  items-center justify-center py-20">
+      <div className="flex  items-start justify-center gap-3 border border-slate-800 bg-slate-50 p-2 shadow-[5px_7px_0px_rgba(0,0,0,1)]   lg:cursor-pointer">
+        <span class="text-green-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </span>
+        Login Success!!!
+        <div className="-mr-1 -mt-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="3"
+            stroke="currentColor"
+            class="h-3 w-3"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UiSimpleAlert;
